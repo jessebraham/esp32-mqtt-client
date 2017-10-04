1cc4a3e699b637157a7a6087885301f69f8b21ea
Jesse Braham <jbraham92@gmail.com>
2017-10-03 18:08:05 -0700

    Additional items added to menuconfig

--- main/component.mk
+++ main/component.mk
@@ -1,6 +1,17 @@
 # TODO:
 # Comment me
 
+COMPONENT_ADD_INCLUDEDIRS := include
+
+
 $(call compile_only_if,$(BLINK_GPIO),esp32-mqtt-client.o)
-$(call compile_only_if,$(SSID),esp32-mqtt-client.o)
-$(call compile_only_if,$(PASSPHRASE),esp32-mqtt-client.o)
+$(call compile_only_if,$(WIFI_SSID),esp32-mqtt-client.o)
+$(call compile_only_if,$(WIFI_PASS),esp32-mqtt-client.o)
+
+$(call compile_only_if,$(MQTT_BROKER),esp32-mqtt-client.o)
+$(call compile_only_if,$(MQTT_PORT),esp32-mqtt-client.o)
+$(call compile_only_if,$(MQTT_USER),esp32-mqtt-client.o)
+$(call compile_only_if,$(MQTT_PASS),esp32-mqtt-client.o)
+
+
+include $(IDF_PATH)/make/component_common.mk
